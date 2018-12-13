545f01f360ace4bfe0d778308e2c19adf486c4d7
oridya <38060133+orihehe@users.noreply.github.com>
2018-12-14 02:08:12 +0900

    2231 분해합 - https://www.acmicpc.net/problem/2231

---
+++ brute force/2231번_분해합.cpp
@@ -0,0 +1,28 @@
+/*
+백준 2231 분해합 (https://www.acmicpc.net/problem/2231)
+*/
+#include <cstdio>
+
+int sbin(int x) {
+	int sum = 0;
+	while (x > 0) {
+		sum += x % 10;
+		x /= 10;
+	}
+	return sum;
+}
+int main() {
+	int n;
+	bool suc = false;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		if ((i + sbin(i)) == n) {
+			printf("%d", i);
+			suc = true;
+			break;
+		}
+	}
+	if (!suc) printf("0");
+
+	return 0;
+}
