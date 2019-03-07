a61bc3cff3a54a704a5533f1956731969347315a
oridya <38060133+orihehe@users.noreply.github.com>
2019-03-07 20:45:02 +0900

    BOJ 11970

---
+++ USACO/11970번_Fence Painting.cpp
@@ -0,0 +1,19 @@
+﻿/*
+BOJ 11970 - Fence Painting
+https://www.acmicpc.net/problem/11970
+*/
+#include <cstdio>
+using namespace std;
+
+/* 🐣🐥 */
+int arr[101];
+int main() {
+	int a, b, c, d, cnt = 0;;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	for (int i = a; i < b; i++) arr[i] = 1;
+	for (int i = c; i < d; i++) arr[i] = 1;
+	for (int i = 0; i <= 100; i++)if (arr[i])cnt++;
+	printf("%d", cnt);
+
+	return 0;
+}
