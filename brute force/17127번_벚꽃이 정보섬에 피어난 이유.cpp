f77adc87a10dd004ca9f491cf2d2cb95f9d7df67
oridya <38060133+orihehe@users.noreply.github.com>
2019-04-06 21:56:07 +0900

    BOJ 17127

---
+++ brute force/17127번_벚꽃이 정보섬에 피어난 이유.cpp
@@ -0,0 +1,31 @@
+﻿/*
+BOJ 17127 - 벚꽃이 정보섬에 피어난 이유
+https://www.acmicpc.net/problem/17127
+*/
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+/* 🐣🐥 */
+int arr[10], mx, n;
+void dfs(int cur, int ans, int cnt) {
+	if (cur == n) {
+		if (cnt == 4) mx = max(mx, ans);
+		return;
+	}
+	int sum = 1;
+	for (int i = cur; i < n; i++) {
+		sum *= arr[i];
+		dfs(i + 1, ans + sum, cnt + 1);
+	}
+}
+int main() {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	dfs(0, 0, 0);
+	printf("%d", mx);
+
+	return 0;
+}
