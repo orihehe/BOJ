82650219e8d0d8ac78d87c6092f0e2da3d21c408
oridya <38060133+orihehe@users.noreply.github.com>
2019-04-17 14:03:36 +0900

    BOJ 11404

---
+++ graph/11404번_플로이드(Floyd-Warshall).cpp
@@ -0,0 +1,39 @@
+﻿/*
+BOJ 11404 - 플로이드
+https://www.acmicpc.net/problem/11404
+*/
+#include <cstdio>
+#include <algorithm>
+#define INF 10000001
+using namespace std;
+
+/* 🐣🐥 */
+int dist[101][101];
+int main() {
+	int n, m, a, b, c;
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		fill(dist[i], dist[i] + n, INF);
+		dist[i][i] = 0;
+	}
+	while (m--) {
+		scanf("%d %d %d", &a, &b, &c);
+		a--, b--;
+		dist[a][b] = min(c, dist[a][b]);
+	}
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", dist[i][j]==INF ? 0 : dist[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
