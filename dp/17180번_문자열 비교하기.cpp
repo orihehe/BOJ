901ef9bc7f40f04847b84b806c3864bfb9809f5a
oridya <38060133+orihehe@users.noreply.github.com>
2019-05-10 22:39:28 +0900

    BOJ 17180

---
+++ dp/17180번_문자열 비교하기.cpp
@@ -0,0 +1,34 @@
+﻿/*
+BOJ 17180 - 문자열 비교하기
+https://www.acmicpc.net/problem/17180
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+/* 🐣🐥 */
+int n, m;
+char a[302], b[302];
+int dp[302][302];
+int go(int ap, int bp) {
+	if (dp[ap][bp] != -1) return dp[ap][bp];
+	int ret = 987654321;
+	// b[bp]문자 늘이기
+	if (ap < n - 1) ret = min(ret, go(ap + 1, bp));
+	// a[ap]문자 늘이기
+	if (bp < m - 1) ret = min(ret, go(ap, bp + 1));
+	// 늘이지 않기
+	if (ap < n - 1 && bp < m - 1) ret = min(ret, go(ap + 1, bp + 1));
+	return dp[ap][bp] = ret + abs(b[bp] - a[ap]);
+}
+int main() {
+	scanf("%d %d", &n, &m);
+	scanf("%s %s", a, b);
+	memset(dp, -1, sizeof(dp));
+	dp[n - 1][m - 1] = abs(a[n - 1] - b[m - 1]);
+	printf("%d", go(0, 0));
+
+	return 0;
+}
