068674dc4f14a84c3b1ab6ab36471eb973223567
oridya <38060133+orihehe@users.noreply.github.com>
2019-05-10 22:29:53 +0900

    BOJ 17175

---
+++ dp/17175번_피보나치는 지겨웡~.cpp
@@ -0,0 +1,26 @@
+﻿/*
+BOJ 17175 - 피보나치는 지겨웡~
+https://www.acmicpc.net/problem/17175
+
+호출 횟수를 메모이제이션
+*/
+#include <cstdio>
+#include <cstring>
+#define mod 1000000007
+using namespace std;
+
+/* 🐣🐥 */
+int dp[51];
+int fibonacci(int n) { // 호출
+	if (dp[n] != -1) return dp[n];
+	return dp[n] = (1LL + fibonacci(n - 2) + fibonacci(n - 1)) % mod;
+}
+int main() {
+	int n;
+	memset(dp, -1, sizeof(dp));
+	dp[0] = dp[1] = 1;
+	scanf("%d", &n);
+	printf("%d", fibonacci(n));
+
+	return 0;
+}
