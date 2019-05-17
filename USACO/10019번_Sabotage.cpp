475c827435f93b7e4c013d0f17b130b3fc59d67d
oridya <38060133+orihehe@users.noreply.github.com>
2019-05-17 15:22:30 +0900

    BOJ 10019

---
+++ USACO/10019번_Sabotage.cpp
@@ -0,0 +1,40 @@
+﻿/*
+BOJ 10019 - Sabotage
+https://www.acmicpc.net/problem/10019
+
+(dp[n]-dp[j]+dp[i])/(n-(j-i)) <= x
+*/
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+/* 🐣🐥 */
+int dp[100001];
+int main() {
+	int n, x;
+	double mid, l, r, mn;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &x);
+		dp[i] = dp[i - 1] + x;
+	}
+	l = 1, r = 10000;
+	for (int j = 0; j < 100; j++) {
+		mid = (l + r) / 2;
+		mn = 10000;
+		bool suc = false;
+		for (int i = 2; i <= n - 1; i++) {
+			mn = min(mn, dp[i - 1] - mid * (i - 1));
+			if (mn + dp[n] - mid * n <= dp[i] - mid * i) {
+				suc = true;
+				break;
+			}
+		}
+		if (suc)
+			r = mid;
+		else l = mid;
+	}
+	printf("%.3lf", r);
+
+	return 0;
+}
