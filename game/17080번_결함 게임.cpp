e9c54ced63dc0a115cfbdbbf751089d0ee1d5cf3
orihehe <38060133+orihehe@users.noreply.github.com>
2019-05-20 20:26:55 +0900

    BOJ 17080

---
+++ game/17080번_결함 게임.cpp
@@ -0,0 +1,16 @@
+﻿/*
+BOJ 17080 - 결함 게임
+https://www.acmicpc.net/problem/17080
+*/
+#include <cstdio>
+using namespace std;
+
+/* 🐣🐥 */
+int main() {
+	int n;
+	scanf("%d", &n);
+	if (n%4==3) printf("2");
+	else printf("1");
+
+	return 0;
+}
