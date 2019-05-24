19585c9c359bbf07f357dad5649bb005a168fa0c
oridya <38060133+orihehe@users.noreply.github.com>
2019-05-24 17:14:40 +0900

    BOJ 17203

---
+++ dp/17203번_ΔEasyMAX.cpp
@@ -0,0 +1,29 @@
+﻿/*
+BOJ 17203 - ∑|ΔEasyMAX|
+https://www.acmicpc.net/problem/17203
+
+부분합으로 구해보자
+*/
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+/* 🐣🐥 */
+int dp[1001], arr[1001];
+int main() {
+	int n, q, a, b;
+	scanf("%d %d", &n, &q);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + abs(arr[i] - arr[i - 1]);
+	}
+	while (q--) {
+		scanf("%d %d", &a, &b);
+		if (a == b) printf("0\n");
+		else printf("%d\n", dp[b] - dp[a]);
+	}
+
+	return 0;
+}
