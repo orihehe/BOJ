4d721b66ae20e1f5d2faf7bd688f009fdbb12fce
oridya <38060133+orihehe@users.noreply.github.com>
2019-05-25 20:43:33 +0900

    소가 길을 건너간 이유

---
+++ 시리즈/소가 길을 건너간 이유/14462번_소가 길을 건너간 이유 8.cpp
@@ -0,0 +1,31 @@
+﻿/*
+BOJ 14462 - 소가 길을 건너간 이유 8
+https://www.acmicpc.net/problem/14462
+
+횡단보도를 설치한 경우 x-1, y-1
+설치하지 않은 경우 x-1,y   x,y-1 의 최댓값을 가져온다.
+*/
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int a[1000], b[1000], dp[1000][1000];
+int go(int x, int y) {
+	if (x<0 || y<0) return 0;
+	if (dp[x][y] != -1) return dp[x][y];
+	dp[x][y] = max({ go(x - 1,y - 1) + (abs(a[x] - b[y]) <= 4),
+		go(x,y - 1),go(x - 1,y) });
+	return dp[x][y];
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i<n; i++) scanf("%d", &a[i]);
+	for (int i = 0; i<n; i++) scanf("%d", &b[i]);
+	memset(dp, -1, sizeof(dp));
+	printf("%d", go(n - 1, n - 1));
+
+	return 0;
+}
---
+++ 시리즈/소가 길을 건너간 이유/14466번_소가 길을 건너간 이유 6.cpp
@@ -0,0 +1,62 @@
+﻿/*
+BOJ 14466 - 소가 길을 건너간 이유 6
+https://www.acmicpc.net/problem/14466
+
+길 정보를 set에 저장해두고, 길을 건너지 않아도 되는 쌍을 묶어 계산해준다.
+*/
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <set>
+#define ll long long
+#define pii pair<int,int>
+using namespace std;
+
+/* 🐣🐥 */
+bool visited[101][101];
+set<pair<pii, pii>> sss;
+vector<int> vec;
+int n, cnt, rgo[4] = { 1,0,-1,0 }, cgo[4] = { 0,1,0,-1 }, bb[101][101];
+void dfs(int r, int c) {
+	visited[r][c] = true;
+	cnt += bb[r][c];
+	for (int i = 0; i < 4; i++) {
+		int nr = r + rgo[i], nc = c + cgo[i];
+		if (nr < 1 || nc < 1 || nr > n || nc > n || visited[nr][nc]) continue;
+		pii a = { r,c }, b = { nr,nc };
+		if (a > b)swap(a, b);
+		if (sss.find({ a,b }) != sss.end()) continue;
+		dfs(nr, nc);
+	}
+}
+int main() {
+	int k, r, sum = 0, x, y;
+	ll ans = 0;
+	pii a, b;
+	scanf("%d %d %d", &n, &k, &r);
+	while (r--) {
+		scanf("%d %d %d %d", &a.first, &a.second, &b.first, &b.second);
+		if (a > b) swap(a, b);
+		sss.insert({ a,b });
+	}
+	while (k--) {
+		scanf("%d %d", &x, &y);
+		bb[x][y]++;
+	}
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			if (!visited[i][j]) {
+				cnt = 0;
+				dfs(i, j);
+				vec.push_back(cnt);
+				sum += cnt;
+			}
+
+	for (int v : vec) {
+		sum -= v;
+		ans += 1LL * v*sum;
+	}
+	printf("%lld", ans);
+
+	return 0;
+}
