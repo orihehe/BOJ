18bbca1ee0c57354c522b2649ac513782864ca1f
orihehe <38060133+orihehe@users.noreply.github.com>
2019-05-27 18:46:21 +0900

    BOJ 11277

---
+++ 2-SAT/11277번_2-SAT - 1.cpp
@@ -0,0 +1,55 @@
+﻿/*
+BOJ 11277 - 2-SAT - 1
+https://www.acmicpc.net/problem/11277
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <stack>
+using namespace std;
+
+/* 🐣🐥 */
+vector<int> vec[41];
+stack<int> sta;
+int n, m, a, b, num[41], cnt;
+bool fn[41], w[41], ans;
+int dfs(int cur) {
+	num[cur] = ++cnt;
+	sta.push(cur);
+
+	int ret = num[cur];
+	for (int v : vec[cur]) {
+		if (!num[v]) ret = min(ret, dfs(v));
+		else if (!fn[v]) ret = min(ret, num[v]);
+	}
+	if (ret == num[cur]) {
+		memset(w, false, sizeof(w));
+		while (true) {
+			int nx = sta.top();
+			sta.pop();
+			if (nx > n && w[nx - n]) ans = true;
+			else if (nx <= n && w[nx + n]) ans = true;
+			w[nx] = true;
+			fn[nx] = true;
+			if (nx == cur) break;
+		}
+	}
+	return ret;
+}
+int main() {
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < m; i++) {
+		scanf("%d %d", &a, &b);
+		if (a < 0) a = -a + n;
+		if (b < 0) b = -b + n;
+		if (a > n) vec[a - n].push_back(b);
+		else vec[a + n].push_back(b);
+		if (b > n) vec[b - n].push_back(a);
+		else vec[b + n].push_back(a);
+	}
+	for (int i = 1; i <= 2 * n; i++) if (!num[i]) dfs(i);
+	printf("%d", !ans);
+
+	return 0;
+}
