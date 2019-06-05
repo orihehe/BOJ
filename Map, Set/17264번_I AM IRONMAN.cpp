4542f2e68a7d0aa1b47287337c0f7e4c84ef5f25
oridya <38060133+orihehe@users.noreply.github.com>
2019-06-06 00:31:18 +0900

    BOJ 17264

---
+++ Map, Set/17264번_I AM IRONMAN.cpp
@@ -0,0 +1,42 @@
+﻿/*
+BOJ 17264 - I AM IRONMAN
+https://www.acmicpc.net/problem/17264
+*/
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+
+/* 🐣🐥 */
+set<string> sss;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m, w, l, g, ans = 0;
+	char ww;
+	string name;
+	cin >> n >> m;
+	cin >> w >> l >> g;
+	for (int i = 0; i < m; i++) {
+		cin >> name >> ww;
+		if (ww == 'W') sss.insert(name);
+	}
+	while (n--) {
+		cin >> name;
+		if (sss.find(name) != sss.end()) {
+			ans += w;
+		}
+		else {
+			ans -= l;
+			ans = ans < 0 ? 0 : ans;
+		}
+		if (ans >= g) {
+			cout << "I AM NOT IRONMAN!!";
+			return 0;
+		}
+	}
+
+	cout << "I AM IRONMAN!!";
+
+	return 0;
+}
