0c1b7f2bb984ec8ee7dfd584ccd1dab5d35e5307
oridya <38060133+orihehe@users.noreply.github.com>
2019-06-08 00:05:23 +0900

    BOJ 15008

---
+++ _etc/15008번_Falling Apart.cpp
@@ -0,0 +1,23 @@
+﻿/*
+BOJ 15008 - Falling Apart
+https://www.acmicpc.net/problem/15008
+*/
+#include <cstdio>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main() {
+	int n, a = 0, b = 0;
+	int arr[16];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+	sort(arr, arr + n, greater<int>());
+	for (int i = 0; i < n; i++) {
+		if (i % 2) b += arr[i];
+		else a += arr[i];
+	}
+	printf("%d %d", a, b);
+
+	return 0;
+}
