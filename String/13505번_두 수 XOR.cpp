f2d78c654e9826a2658d2ccf7bbd50efea02fd8c
oridya <38060133+orihehe@users.noreply.github.com>
2019-06-15 20:39:42 +0900

    Update BOJ 13505

--- String/13505번_두 수 XOR.cpp
+++ String/13505번_두 수 XOR.cpp
@@ -7,68 +7,42 @@ trie를 통해 빠르게 해준다.
 */
 #include <cstdio>
 #include <algorithm>
-#include <map>
 using namespace std;
 
 /* 🐣🐥 */
-map<int, int> mp[3000001];
-map<int, int> sz[3000001];
-int arr[100000];
-int bnr[32], bp;
+int mp[3000000][2];
 int main() {
-	int n, cnt = 0, pre, pp, mu, ans = 0, ori, tmp;
-	bool s0 = false;
+	int n, x, cnt = 0, tt, nw, tmp, ans = 0, b;
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
-	}
-	sort(arr, arr + n);
-	if (arr[0] == 0) s0 = true;
-	for (int i = 0; i < n; i++) {
-		bp = 1;
-		ori = arr[i];
-		while (arr[i] > 0) {
-			bnr[bp++] = arr[i] % 2;
-			arr[i] /= 2;
-		}
-		pre = mu = 0;
-		tmp = -1;
-		pp = bp - 2;
-
-		while (pp >= 1 && (bnr[pp] != 0 || sz[1][pp] == 0)) pp--;
-		if (pp >= 1) tmp = 0;
-		for (int j = pp; j >= 1; j--) {
-			if (mp[pre].find(1) != mp[pre].end() && mp[pre].find(0) != mp[pre].end() && sz[mp[pre][1]][j] != 0 && sz[mp[pre][0]][j] != 0) {
-				if (bnr[j] == 0 && sz[mp[pre][1]][j] != 0) {
-					pre = mp[pre][1];
-					tmp += 1 << (j - 1);
-				}
-				else {
-					pre = mp[pre][0];
-				}
-			}
-			else if (mp[pre].find(1) != mp[pre].end() && sz[mp[pre][1]][j] != 0) {
-				pre = mp[pre][1];
-				tmp += 1 << (j - 1);
+		scanf("%d", &x);
+		nw = tmp = 0;
+		for (int j = 29; j >= 0; j--) {
+			if (i == 0) break;
+			tt = 1 << j;
+			b = x & tt ? 1 : 0;
+			if (mp[nw][!b]) {
+				nw = mp[nw][!b];
+				tmp += !b ? tt : 0;
 			}
 			else {
-				pre = mp[pre][0];
+				nw = mp[nw][b];
+				tmp += b ? tt : 0;
 			}
 		}
-		if (tmp != -1)
-			ans = max(ans, ori^tmp);
-		if (s0) ans = max(ans, ori);
-		pre = 0;
-		for (int j = bp - 1; j >= 1; j--) {
-			if (mp[pre].find(bnr[j]) == mp[pre].end()) {
-				mp[pre][bnr[j]] = ++cnt;
+		if (i != 0) ans = max(ans, x^tmp);
+		nw = 0;
+		for (int j = 29; j >= 0; j--) {
+			tt = 1 << j;
+			b = x & tt ? 1 : 0;
+			if (mp[nw][b]) nw = mp[nw][b];
+			else {
+				mp[nw][b] = ++cnt;
+				nw = cnt;
 			}
-			pre = mp[pre][bnr[j]];
-			sz[pre][j]++;
 		}
 	}
 	printf("%d", ans);
-
 
 	return 0;
 }
