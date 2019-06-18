cb365d3f69ada225be6621a18bf87ba95773c0ca
oridya <38060133+orihehe@users.noreply.github.com>
2019-06-18 22:36:01 +0900

    BOJ 16283

---
+++ _etc/16283번_Farm.cpp
@@ -0,0 +1,22 @@
+﻿/*
+BOJ 16283 - Farm
+https://www.acmicpc.net/problem/16283
+*/
+#include <cstdio>
+using namespace std;
+
+/* 🐣🐥 */
+int main() {
+	int a, b, n, w, aa = -1, bb;
+	scanf("%d %d %d %d", &a, &b, &n, &w);
+	for (int i = 1; i < n; i++) {
+		if (a*i + b * (n - i) == w) {
+			if (aa != -1) return !printf("-1");
+			aa = i, bb = n - i;
+		}
+	}
+	if (aa == -1)printf("-1");
+	else printf("%d %d", aa, bb);
+
+	return 0;
+}
