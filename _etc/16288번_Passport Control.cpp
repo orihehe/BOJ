68e21723af1b72c956491303755c6c10772d2ae2
oridya <38060133+orihehe@users.noreply.github.com>
2019-06-18 22:36:07 +0900

    BOJ 16288

---
+++ _etc/16288번_Passport Control.cpp
@@ -0,0 +1,26 @@
+﻿/*
+BOJ 16288 - Passport Control
+https://www.acmicpc.net/problem/16288
+*/
+#include <cstdio>
+using namespace std;
+
+/* 🐣🐥 */
+int q[101];
+int main() {
+	int n, k, x, j;
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &x);
+		for (j = 0; j < k; j++) {
+			if (q[j] < x) {
+				q[j] = x;
+				break;
+			}
+		}
+		if (j == k) return !printf("NO");
+	}
+	printf("YES");
+
+	return 0;
+}
