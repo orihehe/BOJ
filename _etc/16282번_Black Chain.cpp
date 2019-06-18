363aaf4d43376a101af7ef3413d5f86773bf533d
oridya <38060133+orihehe@users.noreply.github.com>
2019-06-18 22:39:00 +0900

    BOJ 16282

---
+++ _etc/16282번_Black Chain.cpp
@@ -0,0 +1,21 @@
+﻿/*
+BOJ 16282 - Black Chain
+https://www.acmicpc.net/problem/16282
+*/
+#include <cstdio>
+using namespace std;
+
+/* 🐣🐥 */
+int main() {
+	long long n, cc;
+	scanf("%lld", &n);
+	for (int i = 1; i <= 54; i++) {
+		cc = i;
+		for (int j = 0; j <= i; j++) {
+			cc += cc + 1;
+			if (n <= cc) return !printf("%d", i);
+		}
+	}
+
+	return 0;
+}
