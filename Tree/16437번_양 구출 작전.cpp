317bc52e429eb1c518c7d752530970e00683b18c
oridya <38060133+orihehe@users.noreply.github.com>
2019-06-24 21:36:35 +0900

    BOJ 16437

---
+++ Tree/16437번_양 구출 작전.cpp
@@ -0,0 +1,35 @@
+﻿/*
+BOJ 16437 - 양 구출 작전
+https://www.acmicpc.net/problem/16437
+*/
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#define ll long long
+using namespace std;
+
+/* 🐣🐥 */
+vector<int> vec[123457];
+int arr[123457];
+bool wolf[123457];
+ll dfs(int cur) {
+	ll ret = 0;
+	for (int v : vec[cur]) {
+		ret += dfs(v);
+	}
+	if (wolf[cur]) return max(0LL, ret - arr[cur]);
+	else return ret + arr[cur];
+}
+int main() {
+	int n, b;
+	char w;
+	scanf("%d", &n);
+	for (int i = 2; i <= n; i++) {
+		scanf(" %c %d %d", &w, &arr[i], &b);
+		if (w == 'W') wolf[i] = true;
+		vec[b].push_back(i);
+	}
+	printf("%lld", dfs(1));
+
+	return 0;
+}
