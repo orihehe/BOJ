e05c80ef674c16126f24d796067e345ef2dabe5a
orihehe <38060133+orihehe@users.noreply.github.com>
2019-07-04 20:34:48 +0900

    BOJ 11407

---
+++ Flow/11407번_책 구매하기 3.cpp
@@ -0,0 +1,102 @@
+﻿/*
+BOJ 11407 - 책 구매하기 3
+https://www.acmicpc.net/problem/11407
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <queue>
+#define pii pair<int,int>
+#define INF 987654321
+using namespace std;
+
+/* 🐣🐥 */
+struct edg {
+	int u, v, c, op, cost;
+};
+const int S = 201, E = 202;
+vector<edg> eg;
+vector<int> vec[203];
+int dist[203], p[203];
+bool inq[203];
+bool SPFA() {
+	memset(p, -1, sizeof(p));
+	memset(inq, false, sizeof(inq));
+	fill(dist, dist + 203, INF);
+	queue<int> q;
+	dist[S] = 0;
+	inq[S] = true;
+	q.push(S);
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+		inq[cur] = false;
+		for (int v : vec[cur]) {
+			edg i = eg[v];
+			if (i.c > 0 && i.cost + dist[cur] < dist[i.v]) {
+				dist[i.v] = i.cost + dist[cur];
+				p[i.v] = v;
+				if (!inq[i.v]) {
+					inq[i.v] = true;
+					q.push(i.v);
+				}
+			}
+		}
+	}
+	return p[E] != -1;
+}
+pii MCMF() {
+	pii ret = { 0,0 };
+	while (SPFA()) {
+		int mn = INF;
+		for (int i = E; i != S; i = eg[p[i]].u) {
+			mn = min(mn, eg[p[i]].c);
+		}
+		ret.first += mn;
+		for (int i = E; i != S; i = eg[p[i]].u) {
+			ret.second += mn * eg[p[i]].cost;
+			eg[p[i]].c -= mn;
+			eg[eg[p[i]].op].c += mn;
+		}
+	}
+	return ret;
+}
+void aeg(int a, int b, int c, int cost) {
+	int sz = eg.size();
+	edg e1 = { a,b,c,sz + 1,cost };
+	edg e2 = { b,a,0,sz, -cost };
+	eg.push_back(e1);
+	eg.push_back(e2);
+	vec[a].push_back(sz);
+	vec[b].push_back(sz + 1);
+}
+int arr[101][101];
+int main() {
+	int n, m, x;
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &x);
+		aeg(S, i, x, 0);
+	}
+	for (int i = 0; i < m; i++) {
+		scanf("%d", &x);
+		aeg(n + i, E, x, 0);
+	}
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &arr[i][j]);
+		}
+	}
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &x);
+			aeg(j, i + n, arr[i][j], x);
+		}
+	}
+
+	pii ans = MCMF();
+	printf("%d\n%d", ans.first, ans.second);
+
+	return 0;
+}
