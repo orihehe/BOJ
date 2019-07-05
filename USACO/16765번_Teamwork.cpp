662ca1284f7f14f1be56418a66f1112cbf15b1e6
orihehe <38060133+orihehe@users.noreply.github.com>
2019-07-05 15:52:08 +0900

    BOJ 16765

---
+++ USACO/16765번_Teamwork.cpp
@@ -0,0 +1,30 @@
+﻿/*
+BOJ 16765 - Teamwork
+https://www.acmicpc.net/problem/16765
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+/* 🐣🐥 */
+int dp[10001], arr[10001], n, k;
+int dfs(int cur) {
+	if (cur >= n) return 0;
+	if (dp[cur] != -1) return dp[cur];
+	int mx = 0;
+	for (int i = 0; i < k; i++) {
+		if (cur + i >= n) break;
+		mx = max(mx, arr[cur + i]);
+		dp[cur] = max(dp[cur], dfs(cur + i + 1) + (i + 1)*mx);
+	}
+	return dp[cur];
+}
+int main() {
+	scanf("%d %d", &n, &k);
+	memset(dp, -1, sizeof(dp));
+	for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+	printf("%d", dfs(0));
+
+	return 0;
+}
