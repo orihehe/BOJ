8cc7e357a41b96c3b95aaf23e3525f1c94d2f70d
orihehe <38060133+orihehe@users.noreply.github.com>
2019-07-08 23:25:07 +0900

    BOJ 9248

---
+++ String/9248번_Suffix Array (Suffix Array Nlog^2N).cpp
@@ -0,0 +1,60 @@
+﻿/*
+BOJ 9248 - Suffix Array
+https://www.acmicpc.net/problem/9248
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+char st[500002];
+int pos[500002], sa[500002], lcp[500002], d, len;
+bool cmp(int a, int b) {
+	if (pos[a] != pos[b]) return pos[a] < pos[b];
+	a += d, b += d;
+	// 범위를 넘어가면 짧은 문자열이 앞으로
+	return (a < len && b < len) ? pos[a]<pos[b] : a>b;
+}
+void LCP() {
+	for (int i = 0, k = 0; i < len; i++, k = max(0, k - 1)) {
+		if (pos[i] == 0) continue;
+		// 정렬된 접미사배열의 이전 것과 비교
+		for (int j = sa[pos[i] - 1]; st[j + k] == st[i + k]; k++);
+		lcp[pos[i]] = k;
+	}
+}
+void SA() {
+	for (int i = 0; i < len; i++) {
+		pos[i] = st[i];
+		sa[i] = i;
+	}
+	for (d = 1;; d *= 2) {
+		sort(sa, sa + len, cmp);
+		int tmp[500002] = { 0, };
+		for (int i = 0; i < len - 1; i++) {
+			tmp[i + 1] = tmp[i] + cmp(sa[i], sa[i + 1]);
+		}
+		for (int i = 0; i < len; i++) {
+			pos[sa[i]] = tmp[i];
+		}
+		if (tmp[len - 1] == len - 1) break;
+	}
+
+	for (int i = 0; i < len; i++) {
+		pos[sa[i]] = i;
+	}
+	LCP();
+}
+
+int main() {
+	scanf("%s", st);
+	len = strlen(st);
+	SA();
+	for (int i = 0; i < len; i++) printf("%d ", sa[i] + 1);
+	printf("\nx ");
+	for (int i = 1; i < len; i++) {
+		printf("%d ", lcp[i]);
+	}
+
+	return 0;
+}
