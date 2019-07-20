5368c43ade5e89905b3bcc75e03ea35cc9b8d056
oridya <38060133+orihehe@users.noreply.github.com>
2019-07-20 17:18:59 +0900

    트리와 쿼리

---
+++ 시리즈/트리와 쿼리/13510번_트리와 쿼리 1 (HLD).cpp
@@ -0,0 +1,109 @@
+﻿/*
+BOJ 13510 - 트리와 쿼리 1
+https://www.acmicpc.net/problem/13510
+*/
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/* 🐣🐥 */
+struct edge {
+	int u, v, val;
+} arr[100001];
+vector<int> vec[100001];
+int p[100001], tree[262144], hld[100001], ch[100001], num[100001], ori[100001];
+int n, nb, dep[100001];
+int dfs1(int cur, int cnt) {
+	dep[cur] = cnt;
+	for (int v : vec[cur]) {
+		if (!dep[v]) {
+			p[v] = cur;
+			ch[cur] += dfs1(v, cnt + 1);
+		}
+	}
+	return ++ch[cur];
+}
+void dfs2(int cur) {
+	int c = -1;
+	ori[nb] = cur;
+	num[cur] = nb++;
+	for (int v : vec[cur]) {
+		if (p[v] == cur && (c == -1 || ch[c] < ch[v]))
+			c = v;
+	}
+	if (c != -1) {
+		hld[c] = hld[cur];
+		dfs2(c);
+	}
+	for (int v : vec[cur]) {
+		if (v != c && p[v] == cur) {
+			hld[v] = v;
+			dfs2(v);
+		}
+	}
+}
+void init(int r) {
+	hld[r] = p[r] = r;
+	dfs1(r, 1);
+	dfs2(r);
+}
+void update(int idx, int s, int e, int k, int val) {
+	if (s > k || e < k) return;
+	if (s == e) {
+		tree[idx] = val;
+		return;
+	}
+	update(idx * 2, s, (s + e) / 2, k, val);
+	update(idx * 2 + 1, (s + e) / 2 + 1, e, k, val);
+	tree[idx] = max(tree[idx * 2], tree[idx * 2 + 1]);
+}
+int getMax(int idx, int s, int e, int l, int r) {
+	if (e < l || s > r) return 0;
+	if (l <= s && e <= r) return tree[idx];
+	return max(getMax(idx * 2, s, (s + e) / 2, l, r),
+		getMax(idx * 2 + 1, (s + e) / 2 + 1, e, l, r));
+}
+int query(int a, int b) {
+	int ret = 0;
+	while (hld[a] != hld[b]) {
+		if (dep[hld[a]] < dep[hld[b]]) {
+			ret = max(ret, getMax(1, 0, n - 1, num[hld[b]], num[b]));
+			b = p[hld[b]];
+		}
+		else {
+			ret = max(ret, getMax(1, 0, n - 1, num[hld[a]], num[a]));
+			a = p[hld[a]];
+		}
+	}
+	if (dep[a] > dep[b]) swap(a, b);
+	ret = max(ret, getMax(1, 0, n - 1, num[a] + 1, num[b]));
+	return ret;
+}
+int main() {
+	int q, a, b, c;
+	scanf("%d", &n);
+	for (int i = 1; i < n; i++) {
+		scanf("%d %d %d", &a, &b, &c);
+		vec[a].push_back(b);
+		vec[b].push_back(a);
+		arr[i] = { a,b,c };
+	}
+	init(1);
+	for (int i = 1; i < n; i++) {
+		if (dep[arr[i].u] > dep[arr[i].v]) swap(arr[i].u, arr[i].v);
+		update(1, 0, n - 1, num[arr[i].v], arr[i].val);
+	}
+	scanf("%d", &q);
+	while (q--) {
+		scanf("%d %d %d", &a, &b, &c);
+		if (a == 1) {
+			update(1, 0, n - 1, num[arr[b].v], c);
+		}
+		else if (a == 2) {
+			printf("%d\n", query(b, c));
+		}
+	}
+
+	return 0;
+}
---
+++ 시리즈/트리와 쿼리/13511번_트리와 쿼리 2 (LCA).cpp
@@ -0,0 +1,87 @@
+﻿/*
+BOJ 13511 - 트리와 쿼리 2
+https://www.acmicpc.net/problem/13511
+*/
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#define ll long long
+#define pii pair<int,int>
+using namespace std;
+
+/* 🐣🐥 */
+vector<pii> vec[100001];
+int sps[100001][18], dep[100001];
+ll dist[100001];
+void dfs(int cur, int cnt, ll sum) {
+	dep[cur] = cnt;
+	dist[cur] = sum;
+	for (int i = 1; i < 18; i++) {
+		sps[cur][i] = sps[sps[cur][i - 1]][i - 1];
+	}
+	for (pii v : vec[cur]) {
+		if (!dep[v.second]) {
+			sps[v.second][0] = cur;
+			dfs(v.second, cnt + 1, sum + v.first);
+		}
+	}
+}
+int lca(int a, int b) {
+	if (dep[a] > dep[b]) swap(a, b);
+	for (int i = 17; i >= 0; i--) {
+		if (dep[sps[b][i]] >= dep[a]) {
+			b = sps[b][i];
+		}
+	}
+	if (a == b) return a;
+	for (int i = 17; i >= 0; i--) {
+		if (sps[a][i] != sps[b][i]) {
+			a = sps[a][i];
+			b = sps[b][i];
+		}
+	}
+	return sps[a][0];
+}
+int main() {
+	int n, a, b, c, q, d;
+	scanf("%d", &n);
+	for (int i = 1; i < n; i++) {
+		scanf("%d %d %d", &a, &b, &c);
+		vec[a].push_back({ c,b });
+		vec[b].push_back({ c,a });
+	}
+	dfs(1, 1, 0);
+	scanf("%d", &q);
+	while (q--) {
+		scanf("%d %d %d", &a, &b, &c);
+		int lc = lca(b, c);
+		if (a == 1) {
+			printf("%lld\n", dist[b] + dist[c] - 2 * dist[lc]);
+		}
+		else if (a == 2) {
+			scanf("%d", &d);
+			if (dep[b] - dep[lc] + 1 == d) {
+				printf("%d\n", lc);
+				continue;
+			}
+			if (dep[b] - dep[lc] >= d) {
+				d--;
+			}
+			else {
+				d -= dep[b] - dep[lc] + 1;
+				d = dep[c] - dep[lc] - d;
+				b = c;
+			}
+
+			for (int i = 17; i >= 0; i--) {
+				int tt = 1 << i;
+				if (d & tt) {
+					b = sps[b][i];
+				}
+			}
+			printf("%d\n", b);
+		}
+	}
+
+	return 0;
+}
