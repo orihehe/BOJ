57a5280d657ac0007656667e996257298b7690ce
orihehe <38060133+orihehe@users.noreply.github.com>
2019-08-19 22:19:35 +0900

    BOJ 15025

---
+++ _etc/15025번_Judging Moose.cpp
@@ -0,0 +1,20 @@
+﻿/*
+BOJ 15025 - Judging Moose
+https://www.acmicpc.net/problem/15025
+*/
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+/* 🐣🐥 */
+int main() {
+	int l, r;
+	scanf("%d %d", &l, &r);
+	if (l == 0 && r == 0)
+		printf("Not a moose");
+	else if (l != r)
+		printf("Odd %d", max(l, r) * 2);
+	else printf("Even %d", l + r);
+
+	return 0;
+}
