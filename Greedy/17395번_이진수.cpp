bad7675b4bdee0f8ae15c8fca0c9f1c405ffa8a3
orihehe <38060133+orihehe@users.noreply.github.com>
2019-08-21 18:02:56 +0900

    BOJ 17395

---
+++ Greedy/17395번_이진수.cpp
@@ -0,0 +1,36 @@
+﻿/*
+BOJ 17395 - 이진수
+https://www.acmicpc.net/problem/17395
+*/
+#include <cstdio>
+#include <vector>
+#define ll long long
+using namespace std;
+
+/* 🐣🐥 */
+vector<int> vec;
+int main() {
+	ll x, tmp;
+	int n, cnt = 0;
+	scanf("%lld %d", &x, &n);
+	tmp = x;
+	while (x > 0) {
+		vec.push_back(x % 2);
+		if (x % 2) cnt++;
+		x /= 2;
+	}
+	if (cnt < n) return !printf("-1");
+	n--;
+	int sz = vec.size();
+	for (int i = sz - 1; i >= 0; i--) {
+		if (!n) break;
+		if (vec[i]) {
+			n--;
+			tmp ^= 1LL << i;
+			printf("%lld ", tmp);
+		}
+	}
+	printf("0");
+
+	return 0;
+}
