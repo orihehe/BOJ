b399a5e796c0deba8b3d678b045e325c4d8ec350
orihehe <38060133+orihehe@users.noreply.github.com>
2019-09-04 11:51:15 +0900

    BOJ 17402

---
+++ Flow/17402번_시간 끌기.cpp
@@ -0,0 +1,39 @@
+﻿/*
+BOJ 17402 - 시간 끌기
+https://www.acmicpc.net/problem/17402
+*/
+#include <cstdio>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+/* 🐣🐥 */
+vector<int> vec[201];
+int B[201];
+bool visited[201];
+bool dfs(int cur) {
+	visited[cur] = true;
+	for (int v : vec[cur]) {
+		if (B[v] == -1 || !visited[B[v]] && dfs(B[v])) {
+			B[v] = cur;
+			return true;
+		}
+	}
+	return false;
+}
+int main() {
+	int n, m, k, a, b, cnt = 0;
+	scanf("%d %d %d", &n, &m, &k);
+	while (k--) {
+		scanf("%d %d", &a, &b);
+		vec[a].push_back(b);
+	}
+	memset(B, -1, sizeof(B));
+	for (int i = 1; i <= n; i++) {
+		memset(visited, false, sizeof(visited));
+		if (dfs(i)) cnt++;
+	}
+	printf("%d", n + m - cnt);
+
+	return 0;
+}
