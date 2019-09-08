31adaebc8d9a014a26252e15196d9dcfd759a78f
orihehe <38060133+orihehe@users.noreply.github.com>
2019-09-08 19:53:58 +0900

    BOJ 13959

---
+++ Brute Force/13959번_Candle Box.cpp
@@ -0,0 +1,23 @@
+﻿/*
+BOJ 13959 - Candle Box
+https://www.acmicpc.net/problem/13959
+*/
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+/* 🐣🐥 */
+int main() {
+	int d, x, y;
+	int xx = 0;
+	scanf("%d %d %d", &d, &x, &y);
+	for (int i = 4;; i++) {
+		xx += i;
+		int sum = xx;
+		for (int j = 3; j <= i - d; j++) sum += j;
+		if (sum == x + y) {
+			return !printf("%d", x - xx);
+		}
+	}
+	return 0;
+}
