7128f8796a7a7f82286ab064ba2d2e2abcc97b8e
oridya <38060133+orihehe@users.noreply.github.com>
2019-09-15 23:42:03 +0900

    BOJ 15410

---
+++ _etc/15410번_Cakey McCakeFace.cpp
@@ -0,0 +1,33 @@
+﻿/*
+BOJ 15410 - Cakey McCakeFace
+https://www.acmicpc.net/problem/15410
+*/
+#include <cstdio>
+#include <map>
+using namespace std;
+
+/* 🐣🐥 */
+int arr[2001], aa[2001];
+map<int, int> mp;
+int main() {
+	int n, m, mx = 0, ans = 0;
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+	for (int i = 0; i < m; i++)
+		scanf("%d", &aa[i]);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (aa[j] >= arr[i]) mp[aa[j] - arr[i]]++;
+		}
+	}
+	for (auto iter = mp.begin(); iter != mp.end(); iter++) {
+		if (iter->second > mx) {
+			mx = iter->second;
+			ans = iter->first;
+		}
+	}
+	printf("%d", ans);
+
+	return 0;
+}
