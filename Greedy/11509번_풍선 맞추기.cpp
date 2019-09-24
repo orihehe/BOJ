26013d88c5d2d250d2a2b629b926f84c1c50dbec
orihehe <38060133+orihehe@users.noreply.github.com>
2019-09-24 14:51:53 +0900

    BOJ 11509

---
+++ Greedy/11509번_풍선 맞추기.cpp
@@ -0,0 +1,27 @@
+﻿/*
+BOJ 11509 - 풍선 맞추기
+https://www.acmicpc.net/problem/11509
+*/
+#include <cstdio>
+using namespace std;
+
+/* 🐣🐥 */
+int h[1000002];
+int main() {
+	int n, x, ans = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &x);
+		if (h[x + 1]) {
+			h[x + 1]--;
+			h[x]++;
+		}
+		else {
+			ans++;
+			h[x]++;
+		}
+	}
+	printf("%d", ans);
+
+	return 0;
+}
