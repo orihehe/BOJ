d58ac0ce1ecb59c0bd66128241208ea20bab5300
orihehe <38060133+orihehe@users.noreply.github.com>
2019-09-24 13:12:13 +0900

    BOJ 11508

---
+++ Greedy/11508번_2+1 세일.cpp
@@ -0,0 +1,26 @@
+﻿/*
+BOJ 11508 - 2+1 세일
+https://www.acmicpc.net/problem/11508
+*/
+#include <cstdio>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+/* 🐣🐥 */
+int arr[100001];
+int main() {
+	int n;
+	long long ans = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+		ans += arr[i];
+	}
+	sort(arr, arr + n, greater<int>());
+	for (int i = 2; i < n; i += 3)
+		ans -= arr[i];
+	printf("%lld", ans);
+
+	return 0;
+}
