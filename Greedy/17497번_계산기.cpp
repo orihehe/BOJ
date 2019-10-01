f4e5763cff3262a66d692a2e1c9c12408081a18e
orihehe <38060133+orihehe@users.noreply.github.com>
2019-10-01 17:47:08 +0900

    BOJ 17497

---
+++ Greedy/17497번_계산기.cpp
@@ -0,0 +1,33 @@
+﻿/*
+BOJ 17497 - 계산기
+https://www.acmicpc.net/problem/17497
+*/
+#include <cstdio>
+#define ll long long
+using namespace std;
+
+/* 🐣🐥 */
+char ans[100];
+int main() {
+	ll n;
+	int ap = 0;
+	scanf("%lld", &n);
+	if (n % 2) {
+		ans[ap++] = '/';
+		n *= 2;
+	}
+	while (n > 2) {
+		if ((n / 2) % 2) {
+			ans[ap++] = '+';
+			n -= 2;
+		}
+		else {
+			ans[ap++] = '*';
+			n /= 2;
+		}
+	}
+	printf("%d\n[+] ", ap + 1);
+	for (int i = ap - 1; i >= 0; i--) printf("[%c] ", ans[i]);
+
+	return 0;
+}
