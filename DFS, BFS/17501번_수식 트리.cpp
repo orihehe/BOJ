846ccbb7923afd37cbc4851614c6e762fc44b5cb
orihehe <38060133+orihehe@users.noreply.github.com>
2019-10-01 18:02:43 +0900

    BOJ 17501

---
+++ DFS, BFS/17501번_수식 트리.cpp
@@ -0,0 +1,48 @@
+﻿/*
+BOJ 17501 - 수식 트리
+https://www.acmicpc.net/problem/17501
+
+숫자 부분의 부호를 dfs로 구해준다.
+*/
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <functional>
+using namespace std;
+
+/* 🐣🐥 */
+int arr[100001];
+bool buho[200001];
+vector<int> vec[200001];
+int n, x, y;
+void dfs(int cur, bool t) {
+	if (cur <= n) {
+		buho[cur] = t;
+		return;
+	}
+	dfs(vec[cur][0], t);
+	dfs(vec[cur][1], buho[cur] ? !t : t);
+}
+int main() {
+	char cm;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) scanf("%d", &arr[i]);
+	for (int i = n + 1; i <= 2 * n - 1; i++) {
+		scanf(" %c %d %d", &cm, &x, &y);
+		if (cm == '-') buho[i] = true;
+		vec[i].push_back(x);
+		vec[i].push_back(y);
+	}
+	dfs(2 * n - 1, false);
+	int cnt = 0;
+	for (int i = 1; i <= n; i++) {
+		cnt += !buho[i];
+	}
+	int ans = 0;
+	sort(arr + 1, arr + n + 1, greater<int>());
+	for (int i = 1; i <= cnt; i++) ans += arr[i];
+	for (int i = cnt + 1; i <= n; i++) ans -= arr[i];
+	printf("%d", ans);
+
+	return 0;
+}
