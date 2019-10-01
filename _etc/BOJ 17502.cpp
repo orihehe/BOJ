2a801688abaa835fdfdf2186208b44e54df949e8
orihehe <38060133+orihehe@users.noreply.github.com>
2019-10-01 17:37:24 +0900

    BOJ 17502

---
+++ _etc/BOJ 17502.cpp
@@ -0,0 +1,22 @@
+﻿/*
+BOJ 17502 - 클레어와 팰린드롬
+https://www.acmicpc.net/problem/17502
+*/
+#include <cstdio>
+using namespace std;
+
+/* 🐣🐥 */
+char st[101];
+int main() {
+	int n;
+	scanf("%d %s", &n, st);
+	for (int i = 0; i < n / 2; i++) {
+		if (st[i] == '?' && st[n - i - 1] == '?') st[i] = st[n - i - 1] = 'a';
+		else if (st[i] == '?') st[i] = st[n - i - 1];
+		else if (st[n - i - 1] == '?') st[n - i - 1] = st[i];
+	}
+	if (st[n / 2] == '?') st[n / 2] = 'a';
+	printf("%s", st);
+
+	return 0;
+}
