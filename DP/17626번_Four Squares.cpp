b3674e5089bc1dbd5fa4f08f0293283a95484511
oridya <38060133+orihehe@users.noreply.github.com>
2019-10-14 22:42:09 +0900

    BOJ 17626

---
+++ DP/17626번_Four Squares.cpp
@@ -0,0 +1,26 @@
+﻿/*
+BOJ 17626 - Four Squares
+https://www.acmicpc.net/problem/17626
+*/
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+/* 🐣🐥 */
+int wprhq[250], dp[50001];
+int main() {
+	for (int i = 1; i < 250; i++)
+		wprhq[i] = i * i;
+	int n;
+	scanf("%d", &n);
+	fill(dp, dp + n + 1, 50001);
+	dp[0] = 0;
+	for (int i = 1; i < 250; i++) {
+		for (int j = wprhq[i]; j <= n; j++) {
+			dp[j] = min(dp[j], dp[j - wprhq[i]] + 1);
+		}
+	}
+	printf("%d", dp[n]);
+
+	return 0;
+}
