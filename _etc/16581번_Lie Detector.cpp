8be7af23c6a99c17b7a9f2b55df54ecc1715d739
oridya <38060133+orihehe@users.noreply.github.com>
2019-11-02 22:53:49 +0900

    BOJ 16581

---
+++ _etc/16581번_Lie Detector.cpp
@@ -0,0 +1,30 @@
+﻿/*
+BOJ 16581 - Lie Detector
+https://www.acmicpc.net/problem/16581
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* 🐣🐥 */
+char s[2001];
+string arr[100001];
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	bool tt, x;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	tt = true;
+	for (int i = n - 1; i >= 0; i--) {
+		x = arr[i] == "TRUTH";
+		if (!tt) x = !x;
+		tt = x;
+	}
+	cout << (x ? "TRUTH" : "LIE");
+
+	return 0;
+}
