7edc52c19b9d45a2b03e9e33dcf08c6eb8cd4f0a
oridya <38060133+orihehe@users.noreply.github.com>
2019-11-04 22:13:15 +0900

    BOJ 9519

---
+++ _etc/9519번_졸려.cpp
@@ -0,0 +1,38 @@
+﻿/*
+BOJ 9519 - 졸려
+https://www.acmicpc.net/problem/9519
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+/* 🐣🐥 */
+char st[1001], ans[1001];
+int loc[1001][1001];
+int go[1001];
+int main() {
+	int n, len;
+	scanf("%d %s", &n, st);
+	len = strlen(st);
+	int md = 0;
+	for (int i = 0; i < len; i += 2)
+		go[i] = i / 2;
+	for (int i = 1; i < len; i += 2)
+		go[i] = len - i / 2 - 1;
+	for (int i = 1; i < len; i++) {
+		int tmp = i;
+		for (int j = 0;; j++) {
+			md = max(md, j);
+			if (j != 0 && tmp == i && md <= j) break;
+			loc[j][tmp] = i;
+			tmp = go[tmp];
+		}
+	}
+	for (int i = 0; i < len; i++) {
+		ans[i] = st[loc[n % md][i]];
+	}
+	printf("%s", ans);
+
+	return 0;
+}
